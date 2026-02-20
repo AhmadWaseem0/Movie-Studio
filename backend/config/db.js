9302193f8dc0239c3fb744c9e25f347c7215d671
Ahmad <ahmawasim1@gamil.com>
2026-02-20 09:25:52 +0500

    Added MYSQL_URL safety check

--- backend/config/db.js
+++ backend/config/db.js
@@ -23,7 +23,18 @@
 
 // module.exports = db.promise();
 
+// const mysql = require("mysql2");
+
+// const db = mysql.createPool(process.env.MYSQL_URL);
+
+// module.exports = db.promise();
+
 const mysql = require("mysql2");
+
+if (!process.env.MYSQL_URL) {
+    console.error("❌ MYSQL_URL not found in environment variables");
+    process.exit(1);
+}
 
 const db = mysql.createPool(process.env.MYSQL_URL);
 
