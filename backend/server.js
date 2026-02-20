004e698bb24e0136b488d1a6a7006e5f9286a2db
Ahmad <ahmawasim1@gamil.com>
2026-02-20 09:04:16 +0500

    Added DB test and fixed PORT

--- backend/server.js
+++ backend/server.js
@@ -12,14 +12,14 @@ app.use("/api", require("./routes/userRoutes"));
 app.use("/api/movies", movieRoutes);
 app.use("/uploads", express.static("uploads"));
 
-
 db.getConnection()
     .then(() => console.log("✅ MySQL Connected"))
     .catch(err => console.log("❌ DB Error:", err));
-
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Backend running on port ${PORT}`);
 });
+
+
