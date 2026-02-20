3aad0c7fa4099eb80636093d60d9c95ed3c32117
Ahmad <ahmawasim1@gamil.com>
2026-02-20 08:29:06 +0500

    fix start script

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,24 @@
-// config/db.js
+// // config/db.js
+// const mysql = require("mysql2");
+
+// const db = mysql.createPool({
+//     // host: "localhost",
+//     // user: "root",
+//     // password: "",
+//     // database: "moviestudio"
+
+// });
+
+// module.exports = db;
+
 const mysql = require("mysql2");
 
 const db = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "moviestudio"
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
 });
 
-module.exports = db;
+module.exports = db.promise();
