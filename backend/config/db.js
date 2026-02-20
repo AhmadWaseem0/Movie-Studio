62fff58df92e91373b9a59cbd82c9ca21f2ccccc
Ahmad <ahmawasim1@gamil.com>
2026-02-20 09:11:09 +0500

    Switched to MYSQL_URL connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -11,14 +11,20 @@
 
 // module.exports = db;
 
+// const mysql = require("mysql2");
+
+// const db = mysql.createPool({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME,
+//     port: process.env.DB_PORT,
+// });
+
+// module.exports = db.promise();
+
 const mysql = require("mysql2");
 
-const db = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT,
-});
+const db = mysql.createPool(process.env.MYSQL_URL);
 
 module.exports = db.promise();
